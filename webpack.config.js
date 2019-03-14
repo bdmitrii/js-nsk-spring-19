654e80fe2cf8833c79eaa601687c5b2d241a92af
dmitriibelukha <belukhadmit@gmail.com>
2019-03-14 22:49:45 +0700

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,13 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const autoprefixer = require('autoprefixer');
 
 const typeracePath = path.join(__dirname, 'extra', 'typerace', 'client');
+const viewsPath = path.resolve(typeracePath, 'src', 'views');
 
 module.exports = {
   entry: {
-    app: ['@babel/polyfill', path.resolve(typeracePath, 'index.js')]
+    app: ['@babel/polyfill', path.resolve(typeracePath, 'src', 'index.js')],
+    login: path.resolve(typeracePath, 'src', 'login'),
+    registration: path.resolve(typeracePath, 'src', 'registration')
   },
   output: {
     filename: 'bundle.[name].js',
@@ -17,6 +20,9 @@ module.exports = {
     contentBase: path.resolve(typeracePath, 'dist'),
     port: 9000
   },
+  mode: 'production',
+  watch: true,
+  // devtool: 'source-map',
   module: {
     rules: [
       {
@@ -52,11 +58,40 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(typeracePath, 'index.html'),
-      filename: 'index.html'
+      template: path.resolve(viewsPath, 'header.html'),
+      filename: 'views/header.html',
+      inject: false
     }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(viewsPath, 'footer.html'),
+      filename: 'views/footer.html',
+      inject: false
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(viewsPath, 'index.html'),
+      filename: 'views/index.html',
+      inject: 'body',
+      chunks: ['app']
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(viewsPath, 'login.html'),
+      filename: 'views/login.html',
+      inject: 'body',
+      chunks: ['login']
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(viewsPath, 'registration.html'),
+      filename: 'views/registration.html',
+      inject: 'body',
+      chunks: ['registration']
+    }),
+    // new HtmlWebpackPlugin({
+    //   template: path.resolve(viewsPath, 'rating.html'),
+    //   filename: 'views/rating.html',
+    //   inject: false
+    // }),
     new MiniCssExtractPlugin({
-      filename: 'styles.css'
+      filename: 'styles/styles.css'
     })
   ]
 };
